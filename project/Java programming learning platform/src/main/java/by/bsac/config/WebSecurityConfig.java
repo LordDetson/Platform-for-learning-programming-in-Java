ca215987063dd6b500961a627ce628bd6946f7c6
LordDetson <babanin.dima@gmail.com>
2019-05-31 12:29:23 +0300

    Разрешил пользоваться статическими ресурсами по адресу /img/*

--- project/Java programming learning platform/src/main/java/by/bsac/config/WebSecurityConfig.java
+++ project/Java programming learning platform/src/main/java/by/bsac/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/registration", "/activate/*", "/course/img/*").permitAll()
+                    .antMatchers("/", "/registration", "/activate/*", "/img/*").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
